649e54edb560937fa77da805201ba51149b63903
chiaki-124 <sct202304e.s.chiaki@gmail.com>
2023-11-10 03:42:15 +0900

    Update PHP files and Dockerfile

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
 session_start();
 
-function calculate($operand1, $operand2, $operation)
-{ //四則計算
+function calculate($operand1, $operand2, $operation) { //四則計算
     switch ($operation) {
         case '+':
             return $operand1 + $operand2;
@@ -14,6 +13,21 @@ function calculate($operand1, $operand2, $operation)
             if ($operand2 == 0) {
                 return 'Error';
             }
+            return $operand1 / $operand2;
+        default:
+            return null;
+    }
+}
+
+function percent_calculate($operand1, $operand2, $operation){
+    switch ($operation) {
+        case '+':
+            return $operand1 + ( $operand1 * $operand2 );
+        case '-':
+            return $operand1 - ( $operand1 * $operand2 );
+        case '*':
+            return $operand1 * $operand2;
+        case '/':
             return $operand1 / $operand2;
         default:
             return null;
@@ -43,18 +57,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $_SESSION['memory'] = $memory;
             $isNewOperation = true;
             break;
-
+            
         case 'M-':
             $memory -= (float)$currentDisplay;
             $_SESSION['memory'] = $memory;
             $isNewOperation = true;
             break;
-
+            
         case 'RM':
             $display = (string)$memory;
             $isNewOperation = true;
             break;
-
+            
         case 'CM':
             $memory = 0;
             $_SESSION['memory'] = $memory;
@@ -84,14 +98,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $isNewOperation = true;
             }
             break;
-
-        case '0':
+        
         case '00':
-            if ($currentDisplay !== '0') {
+            if ($isNewOperation) {
+                $display = '0';
+                $isNewOperation = false;
+            } elseif ($currentDisplay === '0') {
+                $display = '0';
+            } else {
                 $display .= $input;
             }
             break;
-
+            
         case '+':
         case '-':
         case '*':
@@ -112,48 +130,57 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             break;
 
         case '%':
-            $display = (string)($currentDisplay / 100);
-            $isNewOperation = true;
+            if (isset($_SESSION['operation']) && isset($_SESSION['operand']) && !$isNewOperation){
+                $firstOperand = (float)$_SESSION['operand'];
+                $currentOperand = ((float)$currentDisplay / 100);
+                $operation = $_SESSION['operation'];
+
+                $result = percent_calculate($firstOperand, $currentOperand, $operation);
+                $display = (string)$result;
+
+                unset($_SESSION['operand'], $_SESSION['operation']);
+                $isNewOperation = true;
+            }else{
+                $display = (string)($currentDisplay / 100);
+                $isNewOperation = true;
+            }
             break;
 
         default:
-            if ($isNewOperation) {
-                $display = $input;
-                $isNewOperation = false;
-            } elseif ($currentDisplay === '0') {
-                $display = $input;
-            } else {
-                $display .= $input;
-            }
-            break;
+        if ($isNewOperation) {
+            $display = $input;
+            $isNewOperation = false;
+        } elseif ($currentDisplay === '0') {
+            $display = $input;
+        } else {
+            $display .= $input;
+        }
+        break;
     }
 
     $_SESSION['isNewOperation'] = $isNewOperation;
+    // var_dump($_SESSION);
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="ja">
-
 <head>
     <meta charset="UTF-8">
     <title>電卓アプリ</title>
     <style>
         body {
-            width: 350px;
+            width:350px;
             margin: 0 auto;
             text-align: center;
-            padding-top: 50px;
+            padding-top:50px;
         }
-
         input[type="text"] {
-            width: 318px;
             height: 50px;
             font-size: 32px;
             padding: 10px;
-            margin-bottom: 20px;
+            margin-bottom:20px;
         }
-
         button {
             width: 60px;
             height: 60px;
@@ -163,55 +190,63 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             cursor: pointer;
             float: left;
         }
-
         #equal {
-            width: 130px;
+            width:130px;
         }
     </style>
 </head>
-
 <body>
 
-    <form action="" method="post">
-        <div>
-            <input type="text" name="currentDisplay" readonly value="<?= htmlspecialchars($display, ENT_QUOTES, 'UTF-8') ?>">
-        </div>
-        <div>
-            <button type="submit" name="input" value="CM">CM</button>
-            <button type="submit" name="input" value="RM">RM</button>
-            <button type="submit" name="input" value="M-">M-</button>
-            <button type="submit" name="input" value="M+">M+</button>
-            <button type="submit" name="input" value="CA">CA</button>
-        </div>
-        <div>
-            <button type="submit" name="input" value="7">7</button>
-            <button type="submit" name="input" value="8">8</button>
-            <button type="submit" name="input" value="9">9</button>
-            <button type="submit" name="input" value="%">%</button>
-            <button type="submit" name="input" value="C">C</button>
-        </div>
-        <div>
-            <button type="submit" name="input" value="4">4</button>
-            <button type="submit" name="input" value="5">5</button>
-            <button type="submit" name="input" value="6">6</button>
-            <button type="submit" name="input" value="/">/</button>
-            <button type="submit" name="input" value="*">*</button>
-        </div>
-        <div>
-            <button type="submit" name="input" value="1">1</button>
-            <button type="submit" name="input" value="2">2</button>
-            <button type="submit" name="input" value="3">3</button>
-            <button type="submit" name="input" value="+">+</button>
-            <button type="submit" name="input" value="-">-</button>
-        </div>
-        <div>
-            <button type="submit" name="input" value="0">0</button>
-            <button type="submit" name="input" value="00">00</button>
-            <button type="submit" name="input" value=".">.</button>
-            <button id="equal" type="submit" name="input" value="=">=</button>
-        </div>
-    </form>
+<form action="" method="post">
+    <div>
+        <input type="text" name="currentDisplay" readonly value="<?= htmlspecialchars($display, ENT_QUOTES, 'UTF-8') ?>">
+    </div>
+    <div>
+        <button type="submit" name="input" value="CM">CM</button>
+        <button type="submit" name="input" value="RM">RM</button>
+        <button type="submit" name="input" value="M-">M-</button>
+        <button type="submit" name="input" value="M+">M+</button>
+        <button type="submit" name="input" value="CA">CA</button>
+    </div>
+    <div>
+        <button type="submit" name="input" value="7">7</button>
+        <button type="submit" name="input" value="8">8</button>
+        <button type="submit" name="input" value="9">9</button>
+        <button type="submit" name="input" value="%">%</button>
+        <button type="submit" name="input" value="C">C</button>
+    </div>
+    <div>
+        <button type="submit" name="input" value="4">4</button>
+        <button type="submit" name="input" value="5">5</button>
+        <button type="submit" name="input" value="6">6</button>
+        <button type="submit" name="input" value="/">/</button>
+        <button type="submit" name="input" value="*">*</button>
+    </div>
+    <div>
+        <button type="submit" name="input" value="1">1</button>
+        <button type="submit" name="input" value="2">2</button>
+        <button type="submit" name="input" value="3">3</button>
+        <button type="submit" name="input" value="+">+</button>
+        <button type="submit" name="input" value="-">-</button>
+    </div>
+    <div>
+        <button type="submit" name="input" value="0">0</button>
+        <button type="submit" name="input" value="00">00</button>
+        <button type="submit" name="input" value=".">.</button>
+        <button id="equal" type="submit" name="input" value="=">=</button>
+    </div>
+    <div style="margin-top:30px">
+        <p style="text-align:left">
+            修正        2023/11/9<br>
+            0、％入力の修正<br>
+            %計算はSHARP電卓参考、<br>
+            数値入力後％入力は1/100            
+        </p>
+        
+    
+    </div>
+</form>
 
 </body>
-
 </html>
+
